20ec0acd04b6d6cdbb7b3e8cea40a6ac7132bee9
igor1272 <igor12721345@gmail.com>
2025-04-23 16:38:11 +0300

    Add files via upload

---
+++ задачи.py
@@ -0,0 +1,29 @@
+# 1
+print('задача 1')
+length = 90
+width = 50
+perimeter = 90+50
+print(perimeter)
+
+
+
+print('задача 2')
+money = 10000
+add = 5000
+money += add
+print(money)
+
+
+print('задача 3')
+a = 5000   #ЧАСЫ ВСЕГО
+b = 5000 // 24  #дни
+print(b)
+c = 5000 % 24  #оставшиеся часы
+print(c)
+
+
+print('задача 4')
+a = 100 * 50 * 25 * 4  # вес книги
+b = 1.44 * 1024 * 1024   # объем диска в байтах
+c = a // b     # кол-во книг
+print(c)
